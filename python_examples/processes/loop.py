35b89f27e2e7f59f101d25923a8d3c0b6fae3f71
Julio Melo <julio.melo@imd.ufrn.br>
2019-07-29 13:26:33 -0300

    Adding first python examples

---
+++ python_examples/processes/loop.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import time
+#exemplo para ser usado com system
+
+while True:
+	print 'programa rodando em loop'
+	time.sleep(1); #espera por 1 segundo
+
+print 'Se chegar aqui podemos estar com problema'
---
+++ python_examples/processes/system_py.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+#exemplo do uso de system
+
+os.system("python loop.py")
+
+
