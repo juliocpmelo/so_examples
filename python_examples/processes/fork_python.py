bc99169d64de6210b9757ad55bdd1e9db9b51fd6
Julio Melo <julio.melo@imd.ufrn.br>
2019-07-29 13:45:45 -0300

    Adding new example, fork this time

---
+++ python_examples/processes/fork_python.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+
+print "sou o processo pai!, ainda nao tenho filhos"
+val = os.fork() #fork cria um processo a partir do processo pai
+
+if val == 0 :
+	while True:
+		print 'sou o filho ficarei esperando para sempre aqui'
+		time.sleep(1)
+else:
+	while True:
+		print 'sou o processo pai ficarei esperando para sempre aqui'
+		time.sleep(1)
