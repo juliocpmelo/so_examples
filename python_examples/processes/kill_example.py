482fccea01a258e08a205f2f5b05767add54f476
Julio Melo <julio.melo@imd.ufrn.br>
2019-07-31 10:04:18 -0300

    adding kill and signal examples

---
+++ python_examples/processes/kill_example.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+import signal
+
+pid = input("Informe um pid: ")
+
+#mata o processo
+os.kill(pid, signal.SIGKILL)
+#interropme o processo (msm que ctrl-c)
+#os.kill(pid, signal.SIGINT)
+#pausa o processo
+#os.kill(pid, signal.SIGSTOP)
+#retoma o processo
+#os.kill(pid, signal.SIGCONT)
---
+++ python_examples/processes/signal_example.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+import signal
+import time
+
+#em C essa funçao só recebe um argumento: o número do signal
+def SIGINT_handler(sig_number, sig_stack):
+	print "nunca morrerei com ctrl-c"
+
+#registra a funcao SIGINT_handler para ser executada
+#quando o processo recebe um SIGINT. Assim,
+#o processo nunca morre com ctrl-c.
+signal.signal(signal.SIGINT, SIGINT_handler)
+
+print "Just saying, my pid: " + str(os.getpid())
+
+while True:
+	print "Invincible process"
+	time.sleep(1)
