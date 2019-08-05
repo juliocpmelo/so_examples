20d6fe61eac310c4fb0e95ecbb41edd43f1287e4
Julio Melo <julio.melo@imd.ufrn.br>
2019-08-05 12:42:41 -0300

    adding simple thread example

---
+++ python_examples/threads/simple_thread.py
@@ -0,0 +1,19 @@
+
+import threading
+import time
+import os
+
+def thread_func():
+	t = threading.currentThread()
+	while True :
+		print ("Sou o processo " + str(os.getpid()) + " na thread " + str(t.ident))
+		time.sleep(1)
+
+
+x = threading.Thread(target=thread_func, args=())
+x.start()
+while True :
+	print ("Sou o processo principal " + str(os.getpid()) + " estou executando em paralelo à trhread" + str(x.ident))
+	time.sleep(1)
+
+# x.join()
