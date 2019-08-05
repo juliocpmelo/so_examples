70b1f21d6383c34a4c7683b41181b77498a52cd1
Julio Melo <julio.melo@imd.ufrn.br>
2019-08-05 13:12:21 -0300

    fixing compilation warnings and errors

--- c_examples/threads/thread.c
+++ c_examples/threads/thread.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void* thread_func(void * thread_arg){
 	while(1){
-		printf("Sou o processo %d na thread %p e estou executando em paralelo\n", getpid(), (int)pthread_self());
+		printf("Sou o processo %d na thread %d e estou executando em paralelo\n", getpid(), (int)pthread_self());
 		sleep(1);
 	}
 	return NULL;
@@ -17,7 +19,7 @@ int main(int argc, char** argv){
 	pthread_create(&thread_id, NULL, thread_func, NULL);
 
 	for(;;){
-			printf("Sou o processo principal, %d, e estou executando em paralelo à thread %p\n", getpid(), (int)thread_id);
+			printf("Sou o processo principal, %d, e estou executando em paralelo à thread %d \n", getpid(), (int)thread_id);
 			sleep(1);
 	
 	}
